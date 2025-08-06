6d5bf227e27dc36a09c757dfdd5003a52a30fc79
Varsha Mewada <rainrajput2001@gmail.com>
2025-08-06 15:09:53 +0530

    Add files via upload

---
+++ server/db/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
